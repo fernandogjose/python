2a7529ed5b88d3d2e63d22fd1f9bda0dace0e005
Fernando Jose <fernandogjose@gmail.com>
2023-03-27 22:39:54 -0300

    feature - unittest - rodando o selenium em modo headless

--- src/selenium/test.py
+++ src/selenium/test.py
@@ -2,11 +2,14 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        options = Options()
+        options.headless = True
+        self.driver = webdriver.Firefox(options=options)
 
     def test_search_in_python_org(self):
         driver = self.driver
