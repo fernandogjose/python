b7593da4d7f6612b2e6664823a241637a476266c
fernando.jose <fernando.jose@ferias.co>
2023-04-08 22:06:44 -0300

    feature - app - limpando o app.py

--- src/flask-arquitetura/src/app.py
+++ src/flask-arquitetura/src/app.py
@@ -7,12 +7,4 @@ def create_app():
     app = Flask(__name__)
     configuration.init_app(app)
     configuration.load_extensions(app)
-    # create_database(app)
     return app
-
-
-def create_database(app):
-    print('criado')
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
