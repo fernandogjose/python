da9f7544261c8b16179e1b0e229ea4cc0da0333e
fernando.jose <fernando.jose@ferias.co>
2023-05-03 22:33:02 -0300

    fetaure - test - desenvolvendo uma factory

--- src/aurora-postgresql/tests/domain/services/solicitacao_documento_fiscal_service_test.py
+++ src/aurora-postgresql/tests/domain/services/solicitacao_documento_fiscal_service_test.py
@@ -1,15 +1,24 @@
 import mock
 
 from src.domain.services.solicitacao_documento_fiscal_service import SolicitacaoDocumentoFiscalService
+from src.infra.aurora.repositories.solicitacao_documento_fiscal_repository import SolicitacaoDocumentoFiscalRepository
+
+
+def solicitacao_documento_fiscal_service_instancia_criar(
+        solicitacao_documento_fiscal_repository_mock: SolicitacaoDocumentoFiscalRepository
+) -> SolicitacaoDocumentoFiscalService:
+    return SolicitacaoDocumentoFiscalService(
+        solicitacao_documento_fiscal_repository=solicitacao_documento_fiscal_repository_mock
+    )
 
 
 @mock.patch('src.infra.aurora.repositories.solicitacao_documento_fiscal_repository')
 def test_quando_parametro_correto_deve_adicionar_com_sucesso(solicitacao_documento_fiscal_repository_mock) -> None:
     # arrange
     resultado_esperado = 'teste com moq'
+    solicitacao_documento_fiscal_service = solicitacao_documento_fiscal_service_instancia_criar(
+        solicitacao_documento_fiscal_repository_mock)
     solicitacao_documento_fiscal_repository_mock.adicionar.return_value = 'teste com moq'
-    solicitacao_documento_fiscal_service = SolicitacaoDocumentoFiscalService(
-        solicitacao_documento_fiscal_repository=solicitacao_documento_fiscal_repository_mock)
 
     # act
     resultado = solicitacao_documento_fiscal_service.adicionar()
